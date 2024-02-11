e2294d051c33c153c7686cb716e2774bfe315c76
tanvir243 <travelertanvir@gmail.com>
2024-02-11 18:10:17 +0600

    7 No. completed

---
+++ Solution/7.js
@@ -0,0 +1,8 @@
+
+const country = ['Switzerland', 'Germany', 'France'];
+const city = ['Zurich', 'Berlin', 'Paris'];
+console.log(country);
+console.log(city);
+
+const combinedArray = country.concat(city);
+console.log(combinedArray);
