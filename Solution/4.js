2cf98b3669543c6a954b3dccf3c65a190796e7ad
tanvir243 <travelertanvir@gmail.com>
2024-02-11 13:11:37 +0600

    4 No. completed

---
+++ Solution/4.js
@@ -0,0 +1,10 @@
+
+let tourestPlaces = ['Panam', 'Cox bazer', 'Rangamati'];
+tourestPlaces.push('Sajek');
+console.log(tourestPlaces);
+
+tourestPlaces.push('B-Bariya', 'Tangail');
+console.log(tourestPlaces);
+
+tourestPlaces.pop();
+console.log(tourestPlaces);
