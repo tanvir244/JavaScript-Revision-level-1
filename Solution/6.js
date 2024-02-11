a6f5137581f291399ddcaf6bd3c03d692f46b06d
tanvir243 <travelertanvir@gmail.com>
2024-02-11 14:37:49 +0600

    6 No. completed

---
+++ Solution/6.js
@@ -0,0 +1,8 @@
+
+const professions = ["Web Developer", "Software Enginner", "Cyber Security Expert"];
+const destination = "Lucerne City in Switzerland";
+const ticketPrice = 3280;
+
+console.log(Array.isArray(professions));
+console.log(Array.isArray(destination));
+console.log(Array.isArray(ticketPrice));
