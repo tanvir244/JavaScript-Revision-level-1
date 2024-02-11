c54193b8cd2d46689f26a7a7dc7e310f461cf4e1
tanvir243 <travelertanvir@gmail.com>
2024-02-11 13:33:56 +0600

    5 No. completed

---
+++ Solution/5.js
@@ -0,0 +1,6 @@
+
+const books = ['Git and GitHub', 'Learning JavaScript', 'Python A-Z', 'Problem Solving with JavaScript']
+
+books.includes('Learning JavaScript') ? console.log('Yes, JS related book is available')
+    : console.log('No, not available!')
+    
